1ee819a858ceab455dee1e1affbdd782b4350365
Md. Bakhtiar Galib Siyam <106044768+siyam97cse@users.noreply.github.com>
2023-08-14 13:30:42 +0600

    Create problem1.java

---
+++ problem1.java
@@ -0,0 +1,22 @@
+
+public class problem1
+{
+	public static void main(String[] args) {
+		int num=5;   
+if(num>0)  
+{  
+System.out.println("The number is positive.");  
+}  
+  
+else if(num<0)  
+{  
+System.out.println("The number is negative.");  
+}  
+  
+else  
+{  
+System.out.println("The number is zero.");  
+}  
+	}
+}
+
