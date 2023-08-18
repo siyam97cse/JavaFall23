95df9834de7c5521d55c404688e087a55fb57df5
Md. Bakhtiar Galib Siyam <106044768+siyam97cse@users.noreply.github.com>
2023-08-18 17:39:16 +0600

    Create type1problem4.java

---
+++ type1problem4.java
@@ -0,0 +1,13 @@
+//Vowel or Consonant
+
+public class type1problem4{
+    public static void main(String [] args){
+      char ch = 'I';
+      if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u' 
+      || ch=='A' || ch=='A' || ch=='I' || ch=='O' || ch=='U' ){
+          System.out.println(ch + " is vowel");
+      }else{
+          System.out.println(ch + " is consonant");
+      }
+    }
+}
