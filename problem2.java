a51703482da703a40a2383ae262d43c7044b5cd7
Md. Bakhtiar Galib Siyam <106044768+siyam97cse@users.noreply.github.com>
2023-08-18 17:16:21 +0600

    Create problem2.java

---
+++ problem2.java
@@ -0,0 +1,15 @@
+public class problem2{
+    public static void main(String [] args){
+        int year = 2021;
+        boolean isLeapYear;
+        isLeapYear=(year%4==0);
+        
+        isLeapYear=isLeapYear&&(year%100!=0 || year%400==0);
+        if(isLeapYear){
+            System.out.println(year+" is a leap year.");
+        }
+        else{
+            System.out.println(year+" is not a leap year.");
+        }
+    }
+}
