073623f47d67f9779586867916da42ce1bc3b923
Md. Bakhtiar Galib Siyam <106044768+siyam97cse@users.noreply.github.com>
2023-08-18 17:54:23 +0600

    Create type1problem5.java

---
+++ type1problem5.java
@@ -0,0 +1,12 @@
+//Even of Odd
+
+public class type1problem5{
+    public static void main(String [] args){
+     int num= 20;
+     if(num%2==0){
+         System.out.println(num+ " is even");
+     }else{
+         System.out.println(num+ " is odd");
+     }
+    }
+}
